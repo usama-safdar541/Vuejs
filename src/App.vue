753b712bb37c8b71835f2caa5a2676bdd88eca0b
usama-safdar541 <usama.safdar@devbatch.com>
2023-04-05 15:58:39 +0500

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,52 @@
 
 <template>
   <div>
-<label for="goals"></label>
-<input type="text" v-model="enteredValue">
-<button v-on:click="addGoals">Add Goals</button>
-<ul>
-  <li>{{ goals }}</li>
-</ul>
-<dIsplay></dIsplay>
+    <label for="goals"></label>
+    <input type="text" v-model="enteredValue">
+    <button v-on:click="addGoals">Add Goals</button>
+    <ul>
+      <li>{{ goals }}</li>
+    </ul>
+    <dIsplay></dIsplay>
 
-<div>
-  <h1>{{ title }}</h1>
-  <button v-on:click="add()">Add</button>
-  <button v-on:click="subtract()">Subtract</button>
-  <p>{{ number  }}</p>
-</div>
-
+    <div>
+      <h1>{{ title }}</h1>
+      <button v-on:click="add()">Add</button>
+      <button v-on:click="subtract()">Subtract</button>
+      <p>{{ number }}</p>
+    </div>
+    <nativeobj></nativeobj>
   </div>
 </template>
 <script>
 
 export default {
-  data () {
+  data() {
     return {
       title: "this is my secind Code",
- number: 0,
+      number: 0,
       goals: [],
       enteredValue: ''
     };
   },
+
+
+
+
   methods: {
     addGoals() {
       this.goals.push(this.enteredValue);
-      this.enteredValue=''
+      this.enteredValue = ''
     },
-    add(){
+    add() {
       this.number++;
 
     },
-    subtract(){
-this.number--;
+    subtract() {
+      this.number--;
     }
   }
-  }
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/nativeobj.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <!-- <input type="text" v-bind:value="name" v-on:input="setName($event, 'Safdar') " v-on:keyup.enter="enteredInput"> -->
+        <input type="text" v-model="name" />
+        <!-- <input type="text" v-model="enteredvalue1">
+<button type="submit" v-on:click="setName" >Click</button> -->
+        <p>Your Name:{{ fullname }}</p>
+        <button v-on:click="resetInput"></button>
+    </div>
+</template>
+
+<script>
+// import { METHODS } from 'http';
+
+export default {
+    data() {
+        return {
+            enteredvalue1: "",
+            name: "",
+            fullname: '',
+      // age:26
+    };
+    },
+    //   computed: {
+    //     fullName() {
+    //       console.log("Running");
+    //       if (this.name === "") {
+    //         return "";
+    //       }
+    //       return this.name + "" + "Safdar";
+    //     },
+    //   },
+
+    watch: {
+        name(values) {
+            if (values === '') {
+                this.fullname = '';
+            } else {
+                this.fullname = values + '' + this.lastname;
+            }
+        },
+        lastname(values) {
+            if (values === '') {
+                this.fullname = '';
+            } else {
+                this.fullname = this.name + '' + values;
+            }
+        }
+    },
+    methods: {
+        enteredInput() {
+            this.enteredvalue1 = this.name;
+        },
+
+        resetInput() {
+            this.name = "";
+        },
+        setName(event) {
+            this.name = event.target.value;
+        },
+
+        // setName(){
+        //     this.name.push(this.enteredvalue1);
+        //     this.enteredvalue1=''
+        // }
+    },
+};
+</script>

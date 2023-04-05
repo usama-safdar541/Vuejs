99351d3ab8483f6758a227748f9d1da2b1809d8b
usama-safdar541 <usama.safdar@devbatch.com>
2023-04-05 15:58:08 +0500

    New

--- src/components/dIsplay.vue
+++ src/components/dIsplay.vue
@@ -1,30 +1,30 @@
 <template>
-<div>
-<ul>
-  <h1>{{ title }}</h1>
-    <li>{{ name }}</li>
-    <li>{{ age }}</li>
-    <li>{{ course}}</li>
-</ul>
-</div>
+  <div>
+    <ul>
+      <h1>{{ title }}</h1>
+      <li>{{ name }}</li>
+      <li>{{ age }}</li>
+      <li>{{ course }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 // import display from './dIsplay.vue'
-export default{
-data(){
-    return{
-      title:'working',
-      name:'Usama',
+export default {
+  data() {
+    return {
+      title: 'working',
+      name: 'Usama',
       age: 26,
-      course:'vuejs'
+      course: 'vuejs'
     }
-},
-methods: {
-MYdata(){
-this.name +"And the Age is"+ this.age +"And the course is"+ this.course;
-},
-}
+  },
+  methods: {
+    MYdata() {
+      this.name + "And the Age is" + this.age + "And the course is" + this.course;
+    },
+  }
 
 }
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import display from './components/dIsplay.vue'
-Vue.component('dIsplay', display);
+import Vue from "vue";
+import App from "./App.vue";
+import display from "./components/dIsplay.vue";
+import nativeobjs from "./components/nativeobj.vue";
+// import nativeobj from './components/na'
+Vue.component("dIsplay", display);
+Vue.component("nativeobj", nativeobjs);
 Vue.config.productionTip = false;
 
 new Vue({
   // el:"#app",
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
